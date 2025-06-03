2d77e49e635e3598c4aa91661b77951e2c903bc0
Julia Hadja <juliahadja@gmail.com>
2025-06-03 20:42:44 -0300

    Delete dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Sessao1 from './Componentes/Sessão_1'
-import SessaoArtigo from './Componentes/Sessão_artigo'
 import SessaoEtapas from './Componentes/Sessão Etapas'
 import SessaoSolucao from './Componentes/Sessão solução'
-import Dashboard from './Componentes/Sessão Dashboard'
-import Microexpressao from './Componentes/Sessão Microexpressão'
 import Formulario from './Componentes/Sessão Formulário'
 import Rodape from './Componentes/Rodapé'
 
@@ -22,8 +19,6 @@ function App() {
       <main>
         <SessaoEtapas />
         <SessaoSolucao />
-        <Dashboard />
-        {/* <Microexpressao />  */}
         <Formulario />
       </main>
       <footer>
